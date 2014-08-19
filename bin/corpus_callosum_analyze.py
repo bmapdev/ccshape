e18462a872bdc6a399b1b1570d2cf3e328f0506c
Brandon Ayers <ayersb@ucla.edu>
2014-08-19 15:41:04 -0700

    Added alternate registration flag.  Added default arguments to corpus_callosum_analyze function. Relocated import calls to within function to make incomplete calls from command line more efficient.

--- bin/corpus_callosum_analyze.py
+++ bin/corpus_callosum_analyze.py
@@ -6,9 +6,6 @@ __copyright__ = "Copyright 2013, Shantanu H. Joshi, Brandon Ayers, \
 __email__ = "ayersb@ucla.edu"
 
 import argparse
-import curvematch
-import numpy as np
-from ccshape.corpus_callosum import CorpusCallosum
 import os
 
 def main():
@@ -16,23 +13,27 @@ def main():
     parser.add_argument('subjectIDs', help='Ordered list of subject identifiers')
     parser.add_argument('topCurves', help='Ordered list of paths to top callosal segmentation files')
     parser.add_argument('botCurves', help='Ordered list of paths to bottom callosal segmenation files')
+    parser.add_argument('-odir', dest='odir', help='output directory', required=True)
     parser.add_argument('-templateID', dest='templateID', help='SubjectID associated with curves to be used as template for group registration', default=False, required=False)
     parser.add_argument('-linearTemplate', dest='linearTemplate', help='Do not use elastic matching if template id is specified.', action='store_true', default=False, required=False)
     parser.add_argument('-listInput', dest='listInput', help='Input curves names directly into arguments', action='store_true', default=False, required=False)
     parser.add_argument('-open', dest='open_curves', help='match open curves', action='store_true', default=False, required=False)
     parser.add_argument('-linear', dest='linear', help='use linear matching', action='store_true', default=False, required=False)
-    parser.add_argument('-norotate', dest='norotate', help='do not align rotations', action='store_false', default=True, required=False)
-    parser.add_argument('-noplot', dest='noplot', help='do not save graphical plots', action='store_true', default=False, required=False)
-    parser.add_argument('-odir', dest='odir', help='output directory', required=True)
+    parser.add_argument('-norotate', dest='norotate', help='do not align rotations', action='store_true', default=False,required=False)
+    parser.add_argument('-noplot', dest='noplot', help='do not save graphical plots', action='store_true', default=False,required=False)
     parser.add_argument('-resize', dest='resize', help='resize to the specified number of vertices', required=False, default=100)
+    parser.add_argument('-altThicknessReg', dest='altReg', action='store_true', help='Use alternative method to register thickness values', required=False, default=False)
     args = parser.parse_args()
-    corpus_callosum_analyze(args.subjectIDs, args.topCurves, args.botCurves, args.templateID, args.listInput,
-                            args.open_curves, args.linear, args.norotate, args.noplot, args.odir, args.resize, args.linearTemplate)
+    corpus_callosum_analyze(args.subjectIDs, args.topCurves, args.botCurves, args.odir, args.templateID, args.listInput,
+                            args.open_curves, args.linear, args.norotate, args.noplot, args.resize,
+                            args.linearTemplate, args.altReg)
 
 
-def corpus_callosum_analyze(subject_ids, top_curves, bot_curves, template_id, list_input,
-                            open_curves, linear, no_rotate, no_plot, odir, resize, linear_template_matching):
-
+def corpus_callosum_analyze(subject_ids, top_curves, bot_curves, odir, template_id, list_input=False,
+                            open_curves=False, linear=False, no_rotate=False, no_plot=False, resize=100, linear_template_matching=False, altReg=False):
+    import curvematch
+    import numpy as np
+    from ccshape.corpus_callosum import CorpusCallosum
     if not list_input:
         subject_ids = open(os.path.abspath(subject_ids))
         subject_ids = subject_ids.read().split()
@@ -40,7 +41,7 @@ def corpus_callosum_analyze(subject_ids, top_curves, bot_curves, template_id, li
         top_curves = top_curves.read().split()
         bot_curves = open(os.path.abspath(bot_curves))
         bot_curves = bot_curves.read().split()
-    print 'template_id = ', template_id,'\n\n'
+    print 'template_id = ', template_id, '\n\n'
 
     if template_id:
         if template_id not in subject_ids:
@@ -49,7 +50,8 @@ def corpus_callosum_analyze(subject_ids, top_curves, bot_curves, template_id, li
             os.makedirs(os.path.join(odir, 'template'))
         template_index = subject_ids.index(template_id)
         template_cc = CorpusCallosum(template_id, top_curves[template_index], bot_curves[template_index],
-                                     linear=linear, outdir=os.path.join(odir, 'template'), linear_template_matching=linear_template_matching)
+                                     linear=linear, outdir=os.path.join(odir, 'template'),
+                                     linear_template_matching=linear_template_matching, alt_registration=altReg)
 
         template_cc.compute_thickness()
         template_cc.output_thickness_ucf()
@@ -76,7 +78,7 @@ def corpus_callosum_analyze(subject_ids, top_curves, bot_curves, template_id, li
             os.makedirs(os.path.join(odir, subject_ids[i]))
         current_cc = CorpusCallosum(subject_ids[i], top_curves[i], bot_curves[i], linear=linear,
                                     template_curve=template_curve, outdir=os.path.join(odir, subject_ids[i]),
-                                    linear_template_matching=linear_template_matching)
+                                    linear_template_matching=linear_template_matching, alt_registration=altReg)
         current_cc.compute_thickness()
         current_cc.output_thickness_ucf()
         if not no_plot:

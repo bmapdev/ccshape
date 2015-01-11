8a6274b2e0e9676d7f33decb3247ed55040c80cc
Brandon Ayers <ayersb@ucla.edu>
2015-01-11 13:20:17 -0800

    Added docstring to corpus_callosum_quick function and made small changes to follow PEP 8

--- bin/corpus_callosum_quick.py
+++ bin/corpus_callosum_quick.py
@@ -23,8 +23,18 @@ def main():
 
 
 def corpus_callosum_quick(source_directory, output_directory, test_level, template_id):
+    """ Analyze properly named corpus callosum UCF segmentations by searching source directory for appropriate files, and
+        automatically outputting setup information and analysis output to the output directory.
+
+        File naming is assumed to be of the form:
+        subjectid_top.ucf subjectid_bot.ucf
+        If registration to a template curve is desired it should be specified by subject id.
+            e.g. template_id = subject_j NOT template_id = subject_j_top.ucf
+    """
     if test_level != "min" and test_level != "mid" and test_level != "max":
         raise ValueError("Error! test_lever must be 'min', 'mid' or 'max'")
+
+    # Import models after function has been called to avoid unnecessary file imports on every call from the terminal.
     import corpus_callosum_analyze
     import corpus_callosum_setup_files
     source_directory = os.path.abspath(source_directory)
@@ -45,39 +55,38 @@ def corpus_callosum_quick(source_directory, output_directory, test_level, templa
             os.mkdir(p)
 
     corpus_callosum_setup_files.create_setup_files(source_directory, os.path.join(output_directory, "setup_files"))
-    subjectIDs = os.path.join(output_directory, "setup_files", "subjectIDs.txt")
-    topCurves = os.path.join(output_directory, "setup_files", "top_curve_paths.txt")
-    botCurves = os.path.join(output_directory, "setup_files", "bottom_curve_paths.txt")
-
+    subject_ids = os.path.join(output_directory, "setup_files", "subject_ids.txt")
+    top_curves = os.path.join(output_directory, "setup_files", "top_curve_paths.txt")
+    bot_curves = os.path.join(output_directory, "setup_files", "bottom_curve_paths.txt")
 
     if test_level == "mid":
         if not template_id:
             test_level = "min"
         else:
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "elastic"),
                                                             template_id=False, linear=False, linear_template_matching=False)
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "linear"),
                                                             template_id=False, linear=True, linear_template_matching=False)
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "reg_elastic"),
                                                             template_id, linear=False, linear_template_matching=False)
 
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "reg_linear"),
                                                             template_id, linear=True, linear_template_matching=False)
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "uniform_reg_elastic"),
                                                             template_id, linear=False, linear_template_matching=True)
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "uniform_reg_linear"),
                                                             template_id, linear=True, linear_template_matching=True)
     if test_level == "min":
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "elastic"),
                                                             template_id=False, linear=False, linear_template_matching=False)
-            corpus_callosum_analyze.corpus_callosum_analyze(subjectIDs, topCurves, botCurves,
+            corpus_callosum_analyze.corpus_callosum_analyze(subject_ids, top_curves, bot_curves,
                                                             os.path.join(output_directory, "output_files", "linear"),
                                                             template_id=False, linear_template_matching=False)
 

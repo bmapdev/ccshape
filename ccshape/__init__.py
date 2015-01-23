72c6333b9a21a995f3fc8dfd0da260ca5be979f0
Brandon Ayers <ayersb@ucla.edu>
2015-01-23 10:29:03 -0800

    Renamed module base folder and edited setup file to reflect change change of package name from ccthickness to cc shape

---
+++ ccshape/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/local/epd/bin/python
+"""init.py"""
+__author__ = "Brandon Ayers"
+__copyright__ = "Copyright 2013, Brandon Ayers, Shantanu H. Joshi \
+                 Ahmanson-Lovelace Brain Mapping Center, University of California Los Angeles"
+__email__ = "ayersb@ucla.edu"
+
+__all__ = ['ccshape', 'corpus_callosum']
